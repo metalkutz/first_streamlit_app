5505acbb060476dfd68b5ea68c9ac4defe8eba02
metalkutz <79380336+metalkutz@users.noreply.github.com>
2022-12-18 20:12:52 +0100

    Update streamlit_app.py
    minor fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,4 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 import requests
 fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
